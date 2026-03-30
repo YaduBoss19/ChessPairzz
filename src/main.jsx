58767f5e60b60b075c4cacba7d0647bf17d97ca0
YaduBoss19 <ppyadukrishnan202@gmail.com>
2026-03-31 01:55:27 +0530

    Remove obsolete LiveBroadcastView in favor of Google Sheets sync

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import LiveBroadcastView from './components/LiveBroadcastView.jsx'
-
-const isLiveView = window.location.pathname.startsWith('/live') || window.location.search.includes('?live');
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {isLiveView ? <LiveBroadcastView /> : <App />}
+    <App />
   </StrictMode>,
 )
